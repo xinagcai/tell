3b9644a04fc69a6b347fd088640e1397cfbc613c
hzxiangcai <1298842190@qq.com>
2020-11-27 20:05:56 +0800

    试一试

--- demo1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ demo1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,19 @@
-package com.example.score;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        textView = new TextView(R.layout.)
+
     }
+
 }
